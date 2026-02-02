0c12e16aa4c7b6c6e4ecc6bebb00c09e30f500f9
George_Stav <georgestavropls@gmail.com>
2026-02-02 13:56:53 +0200

    Create initial project

---
+++ src/main/java/gr/george/volosbeerservice/VolosBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package gr.george.volosbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolosBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolosBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gr/george/volosbeerservice/VolosBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.george.volosbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolosBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
